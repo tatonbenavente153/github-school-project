eaae49dd2642630747c6853b46f49e7cea5fa436
tatonbenavente153 <tatonbenavente153@gmail.com>
2025-03-11 18:01:30 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,7 @@
+var studentName = prompt("Please enter your name");
+console.log(`Welcome to our school ${studentName}!`);
+
+function greet(name) {
+  console.log("Hello " + name);
+}
+greet('Jane Doe');
