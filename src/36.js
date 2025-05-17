a3f6418f0d4c90934b1181872796788498238483
tatonbenavente153 <tatonbenavente153@gmail.com>
2025-05-17 03:17:13 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Random JS Code</title>
+</head>
+<body>
+    <h1>A simple school project for GitHub</h1>
+    <p>This is a random JavaScript code.</p>
+</body>
+</html>
